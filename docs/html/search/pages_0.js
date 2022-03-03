7161d1a405a4f7321482c23f2bf9659736854de2
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-03 22:13:40 +0000

    Moved doxy files and regenerated

--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['bug_20list_0',['Bug List',['../bug.html',1,'']]]
+  ['c_20programming_20resources_20for_20avr_20mcu_27s_0',['C Programming Resources for AVR MCU&apos;s',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,30 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "bcdefhilpt",
-  1: "l",
-  2: "hp",
-  3: "il",
-  4: "bcdefit",
-  5: "bl"
+  0: "c",
+  1: "c"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "variables",
-  5: "pages"
+  1: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes",
-  2: "Files",
-  3: "Functions",
-  4: "Variables",
-  5: "Pages"
+  1: "Pages"
 };
 
