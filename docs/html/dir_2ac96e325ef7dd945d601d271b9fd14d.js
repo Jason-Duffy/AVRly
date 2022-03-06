96f099375e57e0342b85a6bf5b1dba39ebab3e36
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-06 21:36:38 +0000

    regen docs

--- docs/html/dir_2ac96e325ef7dd945d601d271b9fd14d.js
+++ docs/html/dir_2ac96e325ef7dd945d601d271b9fd14d.js
@@ -2,7 +2,7 @@ var dir_2ac96e325ef7dd945d601d271b9fd14d =
 [
     [ "ccs811.c", "ccs811_8c.html", "ccs811_8c" ],
     [ "ccs811.h", "ccs811_8h.html", "ccs811_8h" ],
-    [ "i2c.c", "i2c_8c_source.html", null ],
+    [ "i2c.c", "i2c_8c.html", "i2c_8c" ],
     [ "i2c.h", "i2c_8h.html", "i2c_8h" ],
     [ "log_system.c", "ccs81__sensor_2example_2log__system_8c_source.html", null ],
     [ "log_system.h", "ccs81__sensor_2example_2log__system_8h_source.html", null ],
--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -1,16 +1,17 @@
 var searchData=
 [
-  ['i2c_2eh_0',['i2c.h',['../i2c_8h.html',1,'']]],
-  ['i2c_5fread_5fack_1',['i2c_read_ack',['../i2c_8h.html#aa7f5f94b089b26f76a4f9cc95e89a3db',1,'i2c.c']]],
-  ['i2c_5fread_5fno_5fack_2',['i2c_read_no_ack',['../i2c_8h.html#a9e32e9e2acac27300dc4b9463e5d1bed',1,'i2c.c']]],
-  ['i2c_5fsend_3',['i2c_send',['../i2c_8h.html#ade9ea3e8cd58c50b470141c55d20d057',1,'i2c.c']]],
-  ['i2c_5fstart_4',['i2c_start',['../i2c_8h.html#af104fa2713ad2cfe4f993bdd2ec22c46',1,'i2c.c']]],
-  ['i2c_5fstop_5',['i2c_stop',['../i2c_8h.html#ad35d4e4f52ca74b503d5e5e1e0a3f5f3',1,'i2c.c']]],
-  ['i2c_5fwait_5ffor_5fcomplete_6',['i2c_wait_for_complete',['../i2c_8h.html#a61854d6833c0a31f296b84cdcf057a03',1,'i2c.c']]],
-  ['increment_5fcounter_7',['increment_counter',['../structlcd__config__t.html#a848b26ac11cf574fbf6d74a4300f6e7d',1,'lcd_config_t']]],
-  ['init_5fccs811_8',['init_ccs811',['../ccs811_8c.html#ad1f99971115a62fc25a230fbf88861e6',1,'init_ccs811(ccs811_config_t *p_config):&#160;ccs811.c'],['../ccs811_8h.html#ad1f99971115a62fc25a230fbf88861e6',1,'init_ccs811(ccs811_config_t *p_config):&#160;ccs811.c']]],
-  ['init_5fdac_9',['init_dac',['../mcp4812__dac_8c.html#aaf5962070b11ed66e257763c365ad321',1,'init_dac(dac_config_t *p_config):&#160;mcp4812_dac.c'],['../mcp4812__dac_8h.html#aaf5962070b11ed66e257763c365ad321',1,'init_dac(dac_config_t *p_config):&#160;mcp4812_dac.c']]],
-  ['init_5fi2c_10',['init_i2c',['../i2c_8h.html#a17b39f3b4bea70fbb4ca2b377991c788',1,'i2c.c']]],
-  ['init_5flcd_11',['init_lcd',['../driver_2hd44780__lcd_8c.html#a7850604dcf83e9e426e095c50b22fb87',1,'init_lcd(lcd_config_t *p_config):&#160;hd44780_lcd.c'],['../driver_2hd44780__lcd_8h.html#a7850604dcf83e9e426e095c50b22fb87',1,'init_lcd(lcd_config_t *p_config):&#160;hd44780_lcd.c'],['../example_2hd44780__lcd_8c.html#a7850604dcf83e9e426e095c50b22fb87',1,'init_lcd(lcd_config_t *p_config):&#160;hd44780_lcd.c'],['../example_2hd44780__lcd_8h.html#a7850604dcf83e9e426e095c50b22fb87',1,'init_lcd(lcd_config_t *p_config):&#160;hd44780_lcd.c']]],
-  ['init_5fusart_12',['init_usart',['../ccs81__sensor_2example_2usart_8h.html#ad9b40dee04f1b7ced79b74fe3f6cb7fd',1,'init_usart(void):&#160;usart.c'],['../log__system_2example_2usart_8h.html#ad9b40dee04f1b7ced79b74fe3f6cb7fd',1,'init_usart(void):&#160;usart.c'],['../mcp4812__dac_2example_2usart_8h.html#ad9b40dee04f1b7ced79b74fe3f6cb7fd',1,'init_usart(void):&#160;usart.c']]]
+  ['i2c_2ec_0',['i2c.c',['../i2c_8c.html',1,'']]],
+  ['i2c_2eh_1',['i2c.h',['../i2c_8h.html',1,'']]],
+  ['i2c_5fread_5fack_2',['i2c_read_ack',['../i2c_8c.html#aa7f5f94b089b26f76a4f9cc95e89a3db',1,'i2c_read_ack(void):&#160;i2c.c'],['../i2c_8h.html#aa7f5f94b089b26f76a4f9cc95e89a3db',1,'i2c_read_ack(void):&#160;i2c.c']]],
+  ['i2c_5fread_5fno_5fack_3',['i2c_read_no_ack',['../i2c_8c.html#a9e32e9e2acac27300dc4b9463e5d1bed',1,'i2c_read_no_ack(void):&#160;i2c.c'],['../i2c_8h.html#a9e32e9e2acac27300dc4b9463e5d1bed',1,'i2c_read_no_ack(void):&#160;i2c.c']]],
+  ['i2c_5fsend_4',['i2c_send',['../i2c_8c.html#ade9ea3e8cd58c50b470141c55d20d057',1,'i2c_send(uint8_t data):&#160;i2c.c'],['../i2c_8h.html#ade9ea3e8cd58c50b470141c55d20d057',1,'i2c_send(uint8_t data):&#160;i2c.c']]],
+  ['i2c_5fstart_5',['i2c_start',['../i2c_8c.html#af104fa2713ad2cfe4f993bdd2ec22c46',1,'i2c_start(void):&#160;i2c.c'],['../i2c_8h.html#af104fa2713ad2cfe4f993bdd2ec22c46',1,'i2c_start(void):&#160;i2c.c']]],
+  ['i2c_5fstop_6',['i2c_stop',['../i2c_8c.html#ad35d4e4f52ca74b503d5e5e1e0a3f5f3',1,'i2c_stop(void):&#160;i2c.c'],['../i2c_8h.html#ad35d4e4f52ca74b503d5e5e1e0a3f5f3',1,'i2c_stop(void):&#160;i2c.c']]],
+  ['i2c_5fwait_5ffor_5fcomplete_7',['i2c_wait_for_complete',['../i2c_8c.html#a61854d6833c0a31f296b84cdcf057a03',1,'i2c_wait_for_complete(void):&#160;i2c.c'],['../i2c_8h.html#a61854d6833c0a31f296b84cdcf057a03',1,'i2c_wait_for_complete(void):&#160;i2c.c']]],
+  ['increment_5fcounter_8',['increment_counter',['../structlcd__config__t.html#a848b26ac11cf574fbf6d74a4300f6e7d',1,'lcd_config_t']]],
+  ['init_5fccs811_9',['init_ccs811',['../ccs811_8c.html#ad1f99971115a62fc25a230fbf88861e6',1,'init_ccs811(ccs811_config_t *p_config):&#160;ccs811.c'],['../ccs811_8h.html#ad1f99971115a62fc25a230fbf88861e6',1,'init_ccs811(ccs811_config_t *p_config):&#160;ccs811.c']]],
+  ['init_5fdac_10',['init_dac',['../mcp4812__dac_8c.html#aaf5962070b11ed66e257763c365ad321',1,'init_dac(dac_config_t *p_config):&#160;mcp4812_dac.c'],['../mcp4812__dac_8h.html#aaf5962070b11ed66e257763c365ad321',1,'init_dac(dac_config_t *p_config):&#160;mcp4812_dac.c']]],
+  ['init_5fi2c_11',['init_i2c',['../i2c_8c.html#a17b39f3b4bea70fbb4ca2b377991c788',1,'init_i2c(uint32_t bus_speed):&#160;i2c.c'],['../i2c_8h.html#a17b39f3b4bea70fbb4ca2b377991c788',1,'init_i2c(uint32_t bus_speed):&#160;i2c.c']]],
+  ['init_5flcd_12',['init_lcd',['../driver_2hd44780__lcd_8c.html#a7850604dcf83e9e426e095c50b22fb87',1,'init_lcd(lcd_config_t *p_config):&#160;hd44780_lcd.c'],['../driver_2hd44780__lcd_8h.html#a7850604dcf83e9e426e095c50b22fb87',1,'init_lcd(lcd_config_t *p_config):&#160;hd44780_lcd.c'],['../example_2hd44780__lcd_8c.html#a7850604dcf83e9e426e095c50b22fb87',1,'init_lcd(lcd_config_t *p_config):&#160;hd44780_lcd.c'],['../example_2hd44780__lcd_8h.html#a7850604dcf83e9e426e095c50b22fb87',1,'init_lcd(lcd_config_t *p_config):&#160;hd44780_lcd.c']]],
+  ['init_5fusart_13',['init_usart',['../ccs81__sensor_2example_2usart_8h.html#ad9b40dee04f1b7ced79b74fe3f6cb7fd',1,'init_usart(void):&#160;usart.c'],['../log__system_2example_2usart_8h.html#ad9b40dee04f1b7ced79b74fe3f6cb7fd',1,'init_usart(void):&#160;usart.c'],['../mcp4812__dac_2example_2usart_8h.html#ad9b40dee04f1b7ced79b74fe3f6cb7fd',1,'init_usart(void):&#160;usart.c']]]
 ];
