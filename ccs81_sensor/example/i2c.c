ff4ec833dcc8cf7f6612b0711ad003e85d9cb87c
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-06 00:48:09 +0000

    reformatted

--- ccs81_sensor/example/i2c.c
+++ ccs81_sensor/example/i2c.c
@@ -1,42 +1,90 @@
-#include <avr/interrupt.h>
+/******************************************************************************
+ Copyright (c) 2022 by Jason Duffy
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+******************************************************************************/
+
+/**
+ * @file i2c.h
+ * @author Jason Duffy
+ * @date 5th March 2022
+ * @brief I2C communications driver for AVR MCU's.
+ * @bug No known bugs. 
+ * 
+ * This file provides basic I2C protocol comms functionality.
+ * Please note: Blocking waits are used whilst the host waits for hardware to
+ * set bits in registers. 
+ */
+
+
+#include <avr/interrupt.h> 
 
 #include "i2c.h"
 
 
-void init_i2c(void) {
-  TWBR = 32;                               /* set bit rate, see p. 242 */
-                                     /* 8MHz / (16+2*TWBR*1) ~= 100kHz */
-  TWCR |= (1 << TWEN);                                       /* enable */
-  sei();
+/**
+ * SCL frequency = F_CPU / (16 + (2 * TWBR) * PRESCALER)
+ */
+void init_i2c(uint32_t bus_speed)
+{
+  uint32_t base_frequency = (F_CPU / bus_speed);
+  TWBR = (uint8_t)((base_frequency - 16) / (2 * 1)); // Prescaler of 1
+  TWCR |= (1 << TWEN); // Enable
 }
 
-void i2cWaitForComplete(void) {
+void i2c_wait_for_complete(void)
+{
   loop_until_bit_is_set(TWCR, TWINT);
 }
 
-void i2cStart(void) {
+void i2c_start(void)
+{
   TWCR = (_BV(TWINT) | _BV(TWEN) | _BV(TWSTA));
-  i2cWaitForComplete();
+  i2c_wait_for_complete();
 }
 
-void i2cStop(void) {
+void i2c_stop(void)
+{
   TWCR = (_BV(TWINT) | _BV(TWEN) | _BV(TWSTO));
 }
 
-uint8_t i2cReadAck(void) {
-  TWCR = (_BV(TWINT) | _BV(TWEN) | _BV(TWEA));
-  i2cWaitForComplete();
-  return (TWDR);
-}
-
-uint8_t i2cReadNoAck(void) {
-  TWCR = (_BV(TWINT) | _BV(TWEN));
-  i2cWaitForComplete();
-  return (TWDR);
-}
-
-void i2cSend(uint8_t data) {
+void i2c_send(uint8_t data)
+{
   TWDR = data;
   TWCR = (_BV(TWINT) | _BV(TWEN));                  /* init and enable */
-  i2cWaitForComplete();
+  i2c_wait_for_complete();
 }
+
+
+uint8_t i2c_read_ack(void)
+{
+  TWCR = (_BV(TWINT) | _BV(TWEN) | _BV(TWEA));
+  i2c_wait_for_complete();
+  return (TWDR);
+}
+
+uint8_t i2c_read_no_ack(void)
+{
+  TWCR = (_BV(TWINT) | _BV(TWEN));
+  i2c_wait_for_complete();
+  return (TWDR);
+}
+
+
+/*** end of file ***/
--- ccs81_sensor/example/i2c.h
+++ ccs81_sensor/example/i2c.h
@@ -1,26 +1,87 @@
+/******************************************************************************
+ Copyright (c) 2022 by Jason Duffy
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+******************************************************************************/
+
+/**
+ * @file i2c.h
+ * @author Jason Duffy
+ * @date 5th March 2022
+ * @brief I2C communications driver for AVR MCU's.
+ * @bug No known bugs. 
+ * 
+ * This file provides basic I2C protocol comms functionality.
+ * Please note: Blocking waits are used whilst the host waits for hardware to
+ * set bits in registers. 
+ */
+
 // Functions for i2c communication
 #ifndef I_TWO_C_DOT_H
 #define I_TWO_C_DOT_H
 
 #include <avr/io.h>
 
-void init_i2c(void);
-    /* Sets pullups and initializes bus speed to 100kHz (at FCPU=8MHz) */
 
-void i2cWaitForComplete(void);
-                       /* Waits until the hardware sets the TWINT flag */
+/**
+ * Sets pullups and initializes bus speed to desired bus speed.
+ */
+void init_i2c(uint32_t bus_speed);
 
-void i2cStart(void);
-                               /* Sends a start condition (sets TWSTA) */
-void i2cStop(void);
-                                /* Sends a stop condition (sets TWSTO) */
 
-void i2cSend(uint8_t data);
-                   /* Loads data, sends it out, waiting for completion */
+/**
+ * Waits until the hardware sets the TWINT flag.
+ */
+void i2c_wait_for_complete(void);
 
-uint8_t i2cReadAck(void);
-              /* Read in from slave, sending ACK when done (sets TWEA) */
-uint8_t i2cReadNoAck(void);
-              /* Read in from slave, sending NOACK when done (no TWEA) */
 
-#endif
+/**
+ * Sends a start condition (sets TWSTA).
+ */
+void i2c_start(void);
+
+
+/**
+ * Sends a stop condition (sets TWSTO).
+ */
+void i2c_stop(void);
+
+
+/**
+ * Loads data, sends it out, waiting for completion.
+ */
+void i2c_send(uint8_t data);
+
+
+/**
+ * Read in from slave, sending ACK when done (sets TWEA).
+ */
+uint8_t i2c_read_ack(void);
+
+
+/**
+ * Read in from slave, sending NOACK when done (no TWEA).
+ */
+uint8_t i2c_read_no_ack(void);
+
+
+#endif // I_TWO_C_DOT_H
+
+
+/*** end of file ***/
