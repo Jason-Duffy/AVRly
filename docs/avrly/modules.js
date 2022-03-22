a56b2e09fe8cb10f51de5173c299d50f6bbcacb7
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-22 20:39:48 +0000

    docs regen

--- docs/avrly/modules.js
+++ docs/avrly/modules.js
@@ -1,5 +1,5 @@
 var modules =
 [
-    [ "Anatomy of Embedded Firmware", "group__anatomy.html", "group__anatomy" ],
-    [ "Getting Started", "group__getting-started.html", "group__getting-started" ]
+    [ "Getting Started", "group__getting-started.html", "group__getting-started" ],
+    [ "Anatomy of Embedded Firmware", "group__anatomy.html", "group__anatomy" ]
 ];
--- docs/avrly/search/all_a.js
+++ docs/avrly/search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['main_0',['main',['../anatomy-of-embedded-firmware_2layout-and-documentation-examples_2main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;main.c'],['../getting-started_2source_2main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;main.c']]],
+  ['main_0',['main',['../getting-started_2source_2main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;main.c'],['../anatomy-of-embedded-firmware_2layout-and-documentation-examples_2main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;main.c']]],
   ['main_2ec_1',['main.c',['../anatomy-of-embedded-firmware_2layout-and-documentation-examples_2main_8c.html',1,'(Global Namespace)'],['../contributors-guide_2templates_2main_8c.html',1,'(Global Namespace)'],['../getting-started_2source_2main_8c.html',1,'(Global Namespace)']]],
   ['mcp4812_5fdac_2ec_2',['mcp4812_dac.c',['../mcp4812__dac_8c.html',1,'']]],
   ['mcp4812_5fdac_2eh_3',['mcp4812_dac.h',['../mcp4812__dac_8h.html',1,'']]]
