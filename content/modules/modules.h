f3bba76aa0a6ce4bbd2c5cb2d31d99febd009978
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-23 23:26:37 +0000

    added documentation

--- content/modules/modules.h
+++ content/modules/modules.h
@@ -2,8 +2,34 @@
  * \defgroup data_converters Data Converters
  * \brief Data converters brief
  * \details Some details about data converters
- * \defgroup mcp48x2 MCP48x2 DAC
- * \brief Driver for the MCP48x family of DAC chips from Microchip
+ * \defgroup mcp48x2 MCP48x2 DAC Example
+ * \brief Driver for the MCP48x family of DAC chips from Microchip.
  * \ingroup data_converters
  */
 
+/**
+ * \defgroup debug_tools Debug Tools
+ * \brief Debug tools brief
+ * \details Some details about debug tools
+ * \defgroup log_system Log System Example
+ * \brief Logging subsystem to print debug messages over USART to PC.
+ * \ingroup debug_tools
+ */
+
+/**
+ * \defgroup displays Displays
+ * \brief Displays brief
+ * \details Some details about displays
+ * \defgroup hd44780_lcd HD44780 16x2 LCD Example
+ * \brief Driver for the HD44780 based 16x2 liquid display.
+ * \ingroup displays
+ */
+
+ /**
+ * \defgroup sensors Sensors
+ * \brief Sensors brief
+ * \details Some details about sensors
+ * \defgroup ccs811 CCS811 Gas Sensor
+ * \brief Driver for the CCS811 gas sensor. 
+ * \ingroup sensors
+ */
