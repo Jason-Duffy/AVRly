1ee1bb0207e69c72b9db9e1e5825272132fe6955
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-23 20:10:03 +0000

    docs regen

--- docs/avrly/search/all_0.js
+++ docs/avrly/search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['anatomy_20of_20embedded_20firmware_0',['Anatomy of Embedded Firmware',['../group__anatomy.html',1,'(Global Namespace)'],['../md_avrly_projects_anatomy_of_embedded_firmware__anatomy.html',1,'(Global Namespace)']]],
-  ['avrkit_20_2d_20avr_20development_20resources_1',['AVRkit - AVR Development Resources',['../index.html',1,'']]]
+  ['avrly_20_2d_20avr_20development_20resources_1',['AVRly - AVR Development Resources',['../index.html',1,'']]]
 ];
