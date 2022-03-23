48a5a99366d0ab55cf03bfdfb46b9c5d0078e02d
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-23 22:31:58 +0000

    Update modules.h

--- content/modules/modules.h
+++ content/modules/modules.h
@@ -1,6 +1,8 @@
 /**
  * \defgroup data_converters Data Converters
- * Some text about data converters.
  * \defgroup mcp48x2 MCP48x2 DAC
  * \ingroup data_converters
+ * \file mcp48x2_dac.c
+ * \ingroup mcp48x2
  */
+
