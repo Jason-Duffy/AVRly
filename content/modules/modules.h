b50361a15c60e3b3ccbc569f1a65f026c3621612
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-23 23:01:47 +0000

    moved ingroup statement back to source

--- content/modules/modules.h
+++ content/modules/modules.h
@@ -5,11 +5,5 @@
  * \defgroup mcp48x2 MCP48x2 DAC
  * \brief Driver for the MCP48x family of DAC chips from Microchip
  * \ingroup data_converters
- * 
- * \file data-converters/example/mcp48x2_dac.c
- * \ingroup mcp48x2
- * 
- * \file data-converters/example/mcp48x2_dac.h
- * \ingroup mcp48x2
  */
 
