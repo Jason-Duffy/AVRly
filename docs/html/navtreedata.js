22cccc0ab20f80f40e6c562e901556bc66151b45
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-23 23:55:04 +0000

    docs regen

--- docs/html/navtreedata.js
+++ docs/html/navtreedata.js
@@ -25,60 +25,62 @@
 var NAVTREE =
 [
   [ "AVRly - AVR Development Resources", "index.html", [
-    [ "Anatomy of Embedded Firmware", "md_content_anatomy_of_embedded_firmware__anatomy.html", [
-      [ "Software Requirements", "group__getting__started.html#autotoc_md0", [
-        [ "A Source Code Editor", "group__getting__started.html#autotoc_md1", null ],
-        [ "The AVR Toolchain", "group__getting__started.html#autotoc_md2", [
-          [ "MacOS", "group__getting__started.html#autotoc_md3", null ],
-          [ "Linux", "group__getting__started.html#autotoc_md4", [
-            [ "Debian/Ubuntu distributions", "group__getting__started.html#autotoc_md5", null ],
-            [ "Red Hat and Fedora distributions", "group__getting__started.html#autotoc_md6", null ]
+    [ "Getting-started", "md_content_getting_started__getting_started.html", [
+      [ "Software Requirements", "md_content_getting_started__getting_started.html#autotoc_md0", [
+        [ "A Source Code Editor", "md_content_getting_started__getting_started.html#autotoc_md1", null ],
+        [ "The AVR Toolchain", "md_content_getting_started__getting_started.html#autotoc_md2", [
+          [ "MacOS", "md_content_getting_started__getting_started.html#autotoc_md3", null ],
+          [ "Linux", "md_content_getting_started__getting_started.html#autotoc_md4", [
+            [ "Debian/Ubuntu distributions", "md_content_getting_started__getting_started.html#autotoc_md5", null ],
+            [ "Red Hat and Fedora distributions", "md_content_getting_started__getting_started.html#autotoc_md6", null ]
           ] ],
-          [ "Windows", "group__getting__started.html#autotoc_md7", null ]
+          [ "Windows", "md_content_getting_started__getting_started.html#autotoc_md7", null ]
         ] ],
-        [ "A Serial Terminal Program", "group__getting__started.html#autotoc_md8", null ]
+        [ "A Serial Terminal Program", "md_content_getting_started__getting_started.html#autotoc_md8", null ]
       ] ],
-      [ "Hardware Required", "group__getting__started.html#autotoc_md9", [
-        [ "An AVR microcontroller", "group__getting__started.html#autotoc_md10", null ],
-        [ "An ISP Programmer", "group__getting__started.html#autotoc_md11", null ],
-        [ "A USB to Serial Converter", "group__getting__started.html#autotoc_md12", null ],
-        [ "Various USB cables", "group__getting__started.html#autotoc_md13", null ],
-        [ "Breadboards", "group__getting__started.html#autotoc_md14", null ],
-        [ "Power Supply (optional)", "group__getting__started.html#autotoc_md15", null ],
-        [ "Jumpers/Dupont cables", "group__getting__started.html#autotoc_md16", null ],
-        [ "An assortment of components", "group__getting__started.html#autotoc_md17", null ],
-        [ "A multimeter", "group__getting__started.html#autotoc_md18", null ]
+      [ "Hardware Required", "md_content_getting_started__getting_started.html#autotoc_md9", [
+        [ "An AVR microcontroller", "md_content_getting_started__getting_started.html#autotoc_md10", null ],
+        [ "An ISP Programmer", "md_content_getting_started__getting_started.html#autotoc_md11", null ],
+        [ "A USB to Serial Converter", "md_content_getting_started__getting_started.html#autotoc_md12", null ],
+        [ "Various USB cables", "md_content_getting_started__getting_started.html#autotoc_md13", null ],
+        [ "Breadboards", "md_content_getting_started__getting_started.html#autotoc_md14", null ],
+        [ "Power Supply (optional)", "md_content_getting_started__getting_started.html#autotoc_md15", null ],
+        [ "Jumpers/Dupont cables", "md_content_getting_started__getting_started.html#autotoc_md16", null ],
+        [ "An assortment of components", "md_content_getting_started__getting_started.html#autotoc_md17", null ],
+        [ "A multimeter", "md_content_getting_started__getting_started.html#autotoc_md18", null ]
       ] ],
-      [ "Connecting the Hardware", "group__getting__started.html#autotoc_md19", [
-        [ "MCU Connections", "group__getting__started.html#autotoc_md20", null ],
-        [ "Programmer Connections", "group__getting__started.html#autotoc_md21", null ]
+      [ "Connecting the Hardware", "md_content_getting_started__getting_started.html#autotoc_md19", [
+        [ "MCU Connections", "md_content_getting_started__getting_started.html#autotoc_md20", null ],
+        [ "Programmer Connections", "md_content_getting_started__getting_started.html#autotoc_md21", null ]
       ] ],
-      [ "Configuring the Makefile", "group__getting__started.html#autotoc_md22", [
-        [ "MCU, Programmer and Directory variables", "group__getting__started.html#autotoc_md23", [
-          [ "MCU", "group__getting__started.html#autotoc_md24", null ],
-          [ "F_CPU", "group__getting__started.html#autotoc_md25", null ],
-          [ "BAUD", "group__getting__started.html#autotoc_md26", null ],
-          [ "LIBDIR", "group__getting__started.html#autotoc_md27", null ],
-          [ "PROGRAMMER_TYPE", "group__getting__started.html#autotoc_md28", null ],
-          [ "PROGRAMMER_ARGS", "group__getting__started.html#autotoc_md29", null ]
+      [ "Configuring the Makefile", "md_content_getting_started__getting_started.html#autotoc_md22", [
+        [ "MCU, Programmer and Directory variables", "md_content_getting_started__getting_started.html#autotoc_md23", [
+          [ "MCU", "md_content_getting_started__getting_started.html#autotoc_md24", null ],
+          [ "F_CPU", "md_content_getting_started__getting_started.html#autotoc_md25", null ],
+          [ "BAUD", "md_content_getting_started__getting_started.html#autotoc_md26", null ],
+          [ "LIBDIR", "md_content_getting_started__getting_started.html#autotoc_md27", null ],
+          [ "PROGRAMMER_TYPE", "md_content_getting_started__getting_started.html#autotoc_md28", null ],
+          [ "PROGRAMMER_ARGS", "md_content_getting_started__getting_started.html#autotoc_md29", null ]
         ] ],
-        [ "Fuses", "group__getting__started.html#autotoc_md30", [
-          [ "LFUSE", "group__getting__started.html#autotoc_md31", null ],
-          [ "HFUSE", "group__getting__started.html#autotoc_md32", null ],
-          [ "EFUSE", "group__getting__started.html#autotoc_md33", null ]
+        [ "Fuses", "md_content_getting_started__getting_started.html#autotoc_md30", [
+          [ "LFUSE", "md_content_getting_started__getting_started.html#autotoc_md31", null ],
+          [ "HFUSE", "md_content_getting_started__getting_started.html#autotoc_md32", null ],
+          [ "EFUSE", "md_content_getting_started__getting_started.html#autotoc_md33", null ]
         ] ]
       ] ],
-      [ "Flashing the Chip", "group__getting__started.html#autotoc_md34", [
-        [ "Flash Fuses", "group__getting__started.html#autotoc_md35", null ],
-        [ "Flash the Code", "group__getting__started.html#autotoc_md36", [
-          [ "Basic Make Commands:", "group__getting__started.html#autotoc_md37", null ],
-          [ "Advanced Make Commands:", "group__getting__started.html#autotoc_md38", null ]
+      [ "Flashing the Chip", "md_content_getting_started__getting_started.html#autotoc_md34", [
+        [ "Flash Fuses", "md_content_getting_started__getting_started.html#autotoc_md35", null ],
+        [ "Flash the Code", "md_content_getting_started__getting_started.html#autotoc_md36", [
+          [ "Basic Make Commands:", "md_content_getting_started__getting_started.html#autotoc_md37", null ],
+          [ "Advanced Make Commands:", "md_content_getting_started__getting_started.html#autotoc_md38", null ]
         ] ],
-        [ "Verify Makefile Settings", "group__getting__started.html#autotoc_md39", null ],
-        [ "Style Guide", "group__getting__started.html#autotoc_md40", null ],
-        [ "Documentation", "group__getting__started.html#autotoc_md41", null ],
-        [ "Coding Standards", "group__getting__started.html#autotoc_md42", null ]
-      ] ],
+        [ "Verify Makefile Settings", "md_content_getting_started__getting_started.html#autotoc_md39", null ],
+        [ "Style Guide", "md_content_getting_started__getting_started.html#autotoc_md40", null ],
+        [ "Documentation", "md_content_getting_started__getting_started.html#autotoc_md41", null ],
+        [ "Coding Standards", "md_content_getting_started__getting_started.html#autotoc_md42", null ]
+      ] ]
+    ] ],
+    [ "Anatomy of Embedded Firmware", "md_content_anatomy_of_embedded_firmware__anatomy.html", [
       [ "A Word on Doxygen", "md_content_anatomy_of_embedded_firmware__anatomy.html#autotoc_md44", null ],
       [ "Formatting", "md_content_anatomy_of_embedded_firmware__anatomy.html#autotoc_md45", null ],
       [ "Licence", "md_content_anatomy_of_embedded_firmware__anatomy.html#autotoc_md46", null ],
@@ -118,7 +120,7 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "anatomy-of-embedded-firmware_2layout-and-documentation-examples_2filename_8c.html",
-"group__getting__started.html#autotoc_md29"
+"md_content_getting_started__getting_started.html#autotoc_md22"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
