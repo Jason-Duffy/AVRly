9e8b4d1375c26ef7f7a5eea007d87479205be551
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-23 22:56:42 +0000

    docs regen

--- docs/html/group__mcp48x2.js
+++ docs/html/group__mcp48x2.js
@@ -1,6 +1,5 @@
 var group__mcp48x2 =
 [
     [ "main.c", "modules_2data-converters_2mcp48x2__dac_2example_2main_8c.html", null ],
-    [ "mcp48x2_dac.h", "mcp48x2__dac_8h.html", null ],
     [ "mcp48x2", "group__mcp48x2.html#md_content_modules_data_converters_mcp48x2_dac_mcp48x2", null ]
 ];
