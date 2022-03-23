f49dd50de00c1aa0703cee09e934f350ba22ae7e
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-23 22:44:35 +0000

    Update modules.h

--- content/modules/modules.h
+++ content/modules/modules.h
@@ -1,6 +1,9 @@
 /**
  * \defgroup data_converters Data Converters
+ * \brief Data converters brief
+ * \details Some details about data converters
  * \defgroup mcp48x2 MCP48x2 DAC
+ * \brief Driver for the MCP48x family of DAC chips from Microchip
  * \ingroup data_converters
  * \file mcp48x2_dac.c
  * \ingroup mcp48x2
