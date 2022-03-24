ec377107cbeb6f88679426554d1138ddb04d4df4
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-24 00:00:42 +0000

    Update filename.c

--- content/anatomy-of-embedded-firmware/layout-and-documentation-examples/filename.c
+++ content/anatomy-of-embedded-firmware/layout-and-documentation-examples/filename.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- @copyright Copyright (c) 2022 by Your Name Here.
+ @copyright Copyright &copy 2022 by Your Name Here.
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
