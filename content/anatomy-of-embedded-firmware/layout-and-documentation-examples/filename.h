22dcfbaa7250909008f4d351f6cb0997e863c0dd
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-03-24 00:10:01 +0000

    updated copyright notice and licence

--- content/anatomy-of-embedded-firmware/layout-and-documentation-examples/filename.h
+++ content/anatomy-of-embedded-firmware/layout-and-documentation-examples/filename.h
@@ -1,5 +1,5 @@
 /******************************************************************************
- Copyright (c) 2022 by Your Name Here
+ @copyright Copyright © 2022 by Your Name Here.
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
--- content/modules/templates/filename.c
+++ content/modules/templates/filename.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- Copyright (c) by 
+ @copyright Copyright © by
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
