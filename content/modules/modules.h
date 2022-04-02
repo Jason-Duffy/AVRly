6c4935ccf85f1b92723e22fbd071e341a39454a6
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-04-02 20:08:39 +0100

    docs regen

--- content/modules/modules.h
+++ content/modules/modules.h
@@ -1,22 +1,19 @@
 /**
  * @defgroup getting_started Getting Started with Blink
- * @brief A guide for setting up your development environment.
- * @details This module contains the documentation for the files referenced in
- * Getting Started with Blink.
+ * @brief Documentation for the files referenced in Getting Started with Blink.
  */
 
 /**
  * @defgroup anatomy Anatomy of Embedded Firmware
- * @brief Guidelines for layout and style.
- * @details This module contains the documentation for the files referenced in
- * Anatomy of Embedded Firmware.
+ * @brief Documentation for the files referenced in Anatomy of Embedded
+ * Firmware.
  * @see The files can be downloaded from the repo at:
  * https://github.com/Jason-Duffy/AVRly/tree/main/content/anatomy-of-embedded-firmware/layout-and-documentation-examples
  */
 
 /**
  * @defgroup data_converters Data Converters
- * @brief A collection of drivers and example applications for Data Converters.
+ * @brief A collection of guides and code documentation for data converters. 
  * @defgroup mcp48x2 MCP48x2 DAC Example
  * @brief Driver for the MCP48x2 family of DAC chips from Microchip.
  * @ingroup data_converters
@@ -24,8 +21,7 @@
 
 /**
  * @defgroup debug_tools Debug Tools
- * @brief Debug tools brief
- * @details Some details about debug tools
+ * @brief A collection of guides and code documentation for debugging tools.
  * @defgroup log_system Log System Example
  * @brief Logging subsystem to print debug messages over USART to PC.
  * @ingroup debug_tools
@@ -33,17 +29,15 @@
 
 /**
  * @defgroup displays Displays
- * @brief Displays brief
- * @details Some details about displays
+ * @brief A collection of guides and code documentation for displays.
  * @defgroup hd44780_lcd HD44780 16x2 LCD Example
- * @brief Driver for the HD44780 based 16x2 liquid display.
+ * @brief Driver for the HD44780 based 16x2 liquid crystal display.
  * @ingroup displays
  */
 
  /**
  * @defgroup sensors Sensors
- * @brief Sensors brief
- * @details Some details about sensors
+ * @brief A collection of guides and code documentation for sensors.
  * @defgroup ccs811 CCS811 Gas Sensor
  * @brief Driver for the CCS811 gas sensor. 
  * @ingroup sensors
