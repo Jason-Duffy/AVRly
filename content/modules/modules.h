a202b5c27a155b558ce71bf0cf6eef60f4d6cdf4
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-04-02 19:59:43 +0100

    docs regen

--- content/modules/modules.h
+++ content/modules/modules.h
@@ -1,47 +1,50 @@
 /**
- * \defgroup getting_started Getting Started with Blink
- * \brief Getting started brief
- * \details Some details about getting started
+ * @defgroup getting_started Getting Started with Blink
+ * @brief A guide for setting up your development environment.
+ * @details This module contains the documentation for the files referenced in
+ * Getting Started with Blink.
  */
 
 /**
- * \defgroup anatomy Anatomy of Embedded Firmware
- * \brief Anatomy of Embedded Firmware brief
- * \details Some details about Anatomy of Embedded Firmware
+ * @defgroup anatomy Anatomy of Embedded Firmware
+ * @brief Guidelines for layout and style.
+ * @details This module contains the documentation for the files referenced in
+ * Anatomy of Embedded Firmware.
+ * @see The files can be downloaded from the repo at:
+ * https://github.com/Jason-Duffy/AVRly/tree/main/content/anatomy-of-embedded-firmware/layout-and-documentation-examples
  */
 
 /**
- * \defgroup data_converters Data Converters
- * \brief Data converters brief
- * \details Some details about data converters
- * \defgroup mcp48x2 MCP48x2 DAC Example
- * \brief Driver for the MCP48x family of DAC chips from Microchip.
- * \ingroup data_converters
+ * @defgroup data_converters Data Converters
+ * @brief A collection of drivers and example applications for Data Converters.
+ * @defgroup mcp48x2 MCP48x2 DAC Example
+ * @brief Driver for the MCP48x2 family of DAC chips from Microchip.
+ * @ingroup data_converters
  */
 
 /**
- * \defgroup debug_tools Debug Tools
- * \brief Debug tools brief
- * \details Some details about debug tools
- * \defgroup log_system Log System Example
- * \brief Logging subsystem to print debug messages over USART to PC.
- * \ingroup debug_tools
+ * @defgroup debug_tools Debug Tools
+ * @brief Debug tools brief
+ * @details Some details about debug tools
+ * @defgroup log_system Log System Example
+ * @brief Logging subsystem to print debug messages over USART to PC.
+ * @ingroup debug_tools
  */
 
 /**
- * \defgroup displays Displays
- * \brief Displays brief
- * \details Some details about displays
- * \defgroup hd44780_lcd HD44780 16x2 LCD Example
- * \brief Driver for the HD44780 based 16x2 liquid display.
- * \ingroup displays
+ * @defgroup displays Displays
+ * @brief Displays brief
+ * @details Some details about displays
+ * @defgroup hd44780_lcd HD44780 16x2 LCD Example
+ * @brief Driver for the HD44780 based 16x2 liquid display.
+ * @ingroup displays
  */
 
  /**
- * \defgroup sensors Sensors
- * \brief Sensors brief
- * \details Some details about sensors
- * \defgroup ccs811 CCS811 Gas Sensor
- * \brief Driver for the CCS811 gas sensor. 
- * \ingroup sensors
+ * @defgroup sensors Sensors
+ * @brief Sensors brief
+ * @details Some details about sensors
+ * @defgroup ccs811 CCS811 Gas Sensor
+ * @brief Driver for the CCS811 gas sensor. 
+ * @ingroup sensors
  */
