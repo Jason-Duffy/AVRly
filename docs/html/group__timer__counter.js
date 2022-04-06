8c26b4ac75d571536fb04a6b6430ae9b38cc0d92
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-04-06 21:17:20 +0100

    docs regen

--- docs/html/group__timer__counter.js
+++ docs/html/group__timer__counter.js
@@ -1,4 +1,5 @@
 var group__timer__counter =
 [
-    [ "Digital Clock", "group__digital-clock.html", null ]
+    [ "Digital Clock", "group__digital-clock.html", null ],
+    [ "timer-counter", "group__timer__counter.html#md_content_modules_avr_peripheral_features_timer_counter_timer_counter", null ]
 ];
--- docs/html/search/all_10.js
+++ docs/html/search/all_10.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['timer_2dcounter_0',['timer-counter',['../md_content_modules_avr_peripheral_features_timer_counter_timer_counter.html',1,'']]],
+  ['timer_2dcounter_0',['timer-counter',['../group__timer__counter.html',1,'']]],
   ['timer_2fcounter_1',['Timer/Counter',['../group__timer__counter.html',1,'']]],
   ['two_5fline_5fdisplay_2',['two_line_display',['../structlcd__config__t.html#a9ccb959ea17cf7b7f13818e4d606b3af',1,'lcd_config_t']]]
 ];
