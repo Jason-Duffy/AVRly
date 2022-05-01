91f5f1b9faba6e887158ec370b5db82500a3c3a5
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-05-01 16:18:48 +0100

    Create usart.h

---
+++ content/modules/avr-peripheral-features/usart/example/usart.h
@@ -0,0 +1,126 @@
+/******************************************************************************
+ @copyright Copyright © 2022 by Jason Duffy.
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+******************************************************************************/
+
+/**
+ * @file usart.h
+ * @ingroup usart
+ * @author Jason Duffy
+ * @date 15th March 2022
+ * @brief Driver file providing core USART communication between the target MCU
+ * and your PC. This file was adapted from Elliot Williams' Github repo
+ * hexagon5un.
+ * @bug No known bugs. 
+ * @see
+ * github.com/hexagon5un/AVR-Programming/tree/master/AVR-Programming-Library
+ */
+
+
+#ifndef BAUD          // If not defined in Makefile
+#define BAUD  9600    // Set a safe default baud rate
+#endif
+
+#ifndef USART_DOT_H
+#define USART_DOT_H
+
+#include <stdint.h>
+
+// These are defined for convenience
+#define   USART_HAS_DATA   bit_is_set(UCSR0A, RXC0)
+#define   USART_READY      bit_is_set(UCSR0A, UDRE0)
+
+
+/**
+ * Takes the defined BAUD and F_CPU, calculates the bit-clock multiplier,
+ * configures the hardware USART ready for use.
+ */
+void init_usart(void);
+
+
+/**
+ * Utility function to transmit a string.
+ * @param my_string is the string constant youd like to print, and should be
+ * enclosed in "" quotation marks. 
+ */
+void print_string(const char my_string[]);
+
+
+/**
+ * Define a string variable, pass it to this function. The string will contain.
+ * whatever you typed over serial.
+ * @param my_string is a pointer to the first element in the character array
+ * you'd like to store the received message in. 
+ * @param max_length is the maximum number of characters expected.
+ */
+void read_string(char my_string[], uint8_t max_length);
+
+
+/**
+ * Prints a byte out as its 3-digit ascii equivalent.
+ * @param byte is the 8 bits of data to be sent, must be unsigned. 
+ */
+void print_byte(uint8_t byte);
+
+
+/**
+ * Prints a word (16-bits) out as its 5-digit ascii equivalent.
+ * @param word is the 16 bits of data to be sent, must be unsigned.
+ */
+void print_word(uint16_t word);
+
+
+/**
+ * Prints a byte out in 1s and 0s.
+ * @param byte is the 8 bits of data to be sent, must be unsigned.
+ */
+void print_binary_byte(uint8_t byte);
+
+
+// !!
+// TODO: find implementation for this and define difference to function below
+// !!
+/**
+ * Convert nibble to hex.
+ */
+char nibble_to_hex(uint8_t nibble);
+
+
+/**
+ * Convert a nibble to a hex character.
+ */
+char nibble_to_hex_character(uint8_t nibble);
+
+
+/**
+ * Prints a byte out in hexadecimal format.
+ */
+void print_hex_byte(uint8_t byte);
+
+
+/**
+ * Takes in up to three ascii digits, converts them to a byte when press enter.
+ */ 
+uint8_t get_number(void);
+
+
+#endif // USART_DOT_H
+
+/*** end of file ***/
