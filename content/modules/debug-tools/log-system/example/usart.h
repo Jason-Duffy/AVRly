386302467b02b9110cfa7f4b1c2eb58a726b0050
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-05-01 21:39:57 +0100

    Updated to new version

--- content/modules/debug-tools/log-system/example/usart.h
+++ content/modules/debug-tools/log-system/example/usart.h
@@ -26,9 +26,12 @@
  * @author Jason Duffy
  * @date 15th March 2022
  * @brief Driver file providing core USART communication between the target MCU
- * and your PC.
+ * and your PC. This file was adapted from Elliot Williams' Github repo
+ * hexagon5un (link in the see also section below).
+ * NOTE: This driver does involve blocking waits, this may be improved on in a
+ * later version. 
  * @bug No known bugs. 
- * @see
+ * @see https://github.com/hexagon5un/AVR-Programming
  */
 
 
@@ -41,74 +44,91 @@
 
 #include <stdint.h>
 
-//These are defined for convenience
+// These are defined for convenience
 #define   USART_HAS_DATA   bit_is_set(UCSR0A, RXC0)
 #define   USART_READY      bit_is_set(UCSR0A, UDRE0)
 
 
 /**
  * Takes the defined BAUD and F_CPU, calculates the bit-clock multiplier,
- * configures the hardware USART.
+ * configures the hardware USART ready for use.
  */
 void init_usart(void);
 
 
 /**
- * Utility function to transmit an entire string from RAM.
+ * Utility function to transmit a string.
+ * @param my_string is the string constant youd like to print, and should be
+ * enclosed in "" quotation marks. 
  */
-void print_string(const char myString[]);
+void usart_print_string(const char my_string[]);
 
 
 /**
- * Define a string variable, pass it to this function. The string will contain
+ * Define a string variable, pass it to this function. The string will contain.
  * whatever you typed over serial.
+ * @param my_string is a pointer to the first element in the character array
+ * you'd like to store the received message in. 
+ * @param max_length is the maximum number of characters expected.
  */
-void read_string(char myString[], uint8_t maxLength);
+void usart_read_string(char my_string[], uint8_t max_length);
 
 
 /**
  * Prints a byte out as its 3-digit ascii equivalent.
+ * @param byte is the 8 bits of data to be sent, must be unsigned. 
  */
-void print_byte(uint8_t byte);
+void usart_print_byte(uint8_t byte);
+
+
+/**
+ * Prints a byte out as its 1-digit ascii equivalent.
+ * @param byte is the 8 bits of data to be sent, must be unsigned, with a
+ * value of 0-9;
+ */
+void usart_print_decimal_digit(uint8_t byte);
+
+
+/**
+ * Prints a byte out as its 1-character ascii equivalent.
+ * @param byte is the character to be sent.
+ */
+void usart_print_char(char byte);
 
 
 /**
  * Prints a word (16-bits) out as its 5-digit ascii equivalent.
+ * @param word is the 16 bits of data to be sent, must be unsigned.
  */
-void print_word(uint16_t word);
+void usart_print_word(uint16_t word);
 
 
 /**
  * Prints a byte out in 1s and 0s.
+ * @param byte is the 8 bits of data to be sent, must be unsigned.
  */
-void print_binary_byte(uint8_t byte);
-
-
-// !!
-// TODO: find implementation for this and define difference to function below
-// !!
-/**
- * Convert nibble to hex.
- */
-char nibble_to_hex(uint8_t nibble);
+void usart_print_binary_byte(uint8_t byte);
 
 
 /**
- * Convert nibble to hex character.
+ * Convert a nibble to a hex character.
+ * @param nibble is the 4 bits of data to be sent, must be unsigned. 
  */
-char nibble_to_hex_character(uint8_t nibble);
+char usart_nibble_to_hex_character(uint8_t nibble);
 
 
 /**
- * Prints a byte out in hexadecimal.
+ * Prints a byte out in hexadecimal format.
+ * @param byte is the 8 bits of data to be sent, must be unsigned. 
  */
-void print_hex_byte(uint8_t byte);
+void usart_print_hex_byte(uint8_t byte);
 
 
 /**
  * Takes in up to three ascii digits, converts them to a byte when press enter.
+ * @returns an unsigned 8 bit integer is returned - this is the data received. 
  */ 
-uint8_t get_number(void);
+uint8_t usart_get_number(void);
 
 
 #endif // USART_DOT_H
