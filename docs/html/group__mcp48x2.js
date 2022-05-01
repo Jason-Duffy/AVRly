497417543a0209cd3232a77e5e47c6fd921f4d3b
Jason <55949529+Jason-Duffy@users.noreply.github.com>
2022-05-01 15:22:08 +0100

    docs regen

--- docs/html/group__mcp48x2.js
+++ docs/html/group__mcp48x2.js
@@ -8,5 +8,7 @@ var group__mcp48x2 =
     [ "mcp48x2_dac.c", "mcp48x2__dac_8c.html", null ],
     [ "mcp48x2_dac.h", "mcp48x2__dac_8h.html", null ],
     [ "pin_defines.h", "modules_2data-converters_2mcp48x2__dac_2example_2pin__defines_8h.html", null ],
+    [ "usart.c", "data-converters_2mcp48x2__dac_2example_2usart_8c.html", null ],
+    [ "usart.h", "data-converters_2mcp48x2__dac_2example_2usart_8h.html", null ],
     [ "README", "group__mcp48x2.html#md_content_modules_data_converters_mcp48x2_dac_README", null ]
 ];
